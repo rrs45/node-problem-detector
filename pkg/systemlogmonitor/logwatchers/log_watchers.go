93fd2d4c0b81c0f847fe246187e55bb3f5a6e5ff
rrs45 <38927084+rrs45@users.noreply.github.com>
2019-02-19 15:26:07 -0800

    Update log_watchers.go

--- pkg/systemlogmonitor/logwatchers/log_watchers.go
+++ pkg/systemlogmonitor/logwatchers/log_watchers.go
@@ -30,9 +30,23 @@ func registerLogWatcher(name string, create types.WatcherCreateFunc) {
 	createFuncs[name] = create
 }
 
+func registerSensuLogWatcher(name string, create types.SensuWatcherCreateFunc) {
+	createFuncs[name] = create
+}
+
 // GetLogWatcherOrDie get a log watcher based on the passed in configuration.
 // The function panics when encounters an error.
 func GetLogWatcherOrDie(config types.WatcherConfig) types.LogWatcher {
+	create, ok := createFuncs[config.Plugin]
+	if !ok {
+		glog.Fatalf("No create function found for plugin %q", config.Plugin)
+	}
+	glog.Infof("Use log watcher of plugin %q", config.Plugin)
+	return create(config)
+}
+
+
+func GetSensuLogWatcherOrDie(config types.WatcherConfig) types.SensuLogWatcher {
 	create, ok := createFuncs[config.Plugin]
 	if !ok {
 		glog.Fatalf("No create function found for plugin %q", config.Plugin)
